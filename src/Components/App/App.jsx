7adc966794b0990fbe4a0f2b6d4db1ebd04b4bcb
Alex Digrazia <alexndigrazia@hotmail.com>
2023-06-20 03:20:20 -0700

    added multiple source tags of different file formats to increase cross browser compatibility for video files

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -6,11 +6,20 @@ function App() {
       <div className="wrapper">
         <div className="menu"></div>
         <div className="video-panel">
-          <video 
-            className="video" 
-            src="https://drx4s5overzyz.cloudfront.net/Demo%20(4).mp4"
-            controls
-            ></video>
+          <video className="video" controls>
+            <source
+              src="https://drx4s5overzyz.cloudfront.net/Demo%20(4).mp4"
+              type="video/mp4"
+            />
+            <source
+              src="https://dyw6s08cwb4dw.cloudfront.net/Demo4.webm"
+              type="video/webm"
+            />
+            <source
+              src="https://d2981qw34z29mg.cloudfront.net/Demo4.ogv"
+              type="video/ogg"
+            />
+          </video>
         </div>
       </div>
     </>
@@ -20,4 +29,3 @@ function App() {
 export default App;
 
 // src="https://d31u5qrj5rhaye.cloudfront.net/Demo.MOV"
-
