6b9ed0bf8d3e465ecfe5b0102e2bfdb2ea4dd702
Alex Digrazia <alexndigrazia@hotmail.com>
2024-08-23 19:19:38 -0700

    simplified <VideoTitle /> to only set CurrentVideoId on parent Context and set localStorage

--- src/Components/VideoTitle/VideoTitle.tsx
+++ src/Components/VideoTitle/VideoTitle.tsx
@@ -1,27 +1,22 @@
 import { useNavigate } from "react-router-dom";
-import { Requests } from "../../API/Requests";
 import { useVideoContext } from "../../Providers/videoProvider";
 
 type TVideoTitleProps = {
   title: string;
-  filename: string;
   id: number;
 };
 
 const VideoTitle = ({ title, id }: TVideoTitleProps) => {
-  const { setCurrentVideoFile } = useVideoContext();
+  const { setCurrentVideoId } = useVideoContext();
   const navigate = useNavigate();
   return (
     <>
       <div
         className="video-title"
         onClick={() => {
-          Requests.getVideoById(id)
-            .then((res) => {
-              localStorage.setItem("lastVideoDisplayed-id", res.id);
-              setCurrentVideoFile(res);
-            })
-            .then(() => navigate(`/app/${id}`));
+          localStorage.setItem("videoLastWatched_id", id.toString());
+          setCurrentVideoId(id);
+          navigate(`/app/${id}`);
         }}
       >
         {title}
