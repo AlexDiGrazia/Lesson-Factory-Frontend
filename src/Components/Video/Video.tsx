cf3d8aeaaba4af2b06df7833bc8174999bff7d09
Alex Digrazia <alexndigrazia@hotmail.com>
2024-08-23 19:18:47 -0700

    Simplified <Video/> component to only handle displaying videos passed to it from parent Context

--- src/Components/Video/Video.tsx
+++ src/Components/Video/Video.tsx
@@ -1,27 +1,20 @@
 // import ReactPlayer from "react-player/youtube";
 
-import { useEffect, useState } from "react";
-import { TVideo, useVideoContext } from "../../Providers/videoProvider";
-import { Requests } from "../../API/Requests";
+import { useVideoContext } from "../../Providers/videoProvider";
 
 const Video = () => {
   const { currentVideoFile } = useVideoContext();
-  const [display, setDisplay] = useState<TVideo>();
-
-  useEffect(() => {
-    Requests.getVideoById(
-      currentVideoFile.id ||
-        Number(localStorage.getItem("lastVideoDisplayed-id"))
-    ).then(setDisplay);
-  }, [currentVideoFile]);
 
   return (
     <>
-      {display && (
-        <video key={display.filename} className="video" playsInline controls>
-          <source src={display.filename} type="video/mp4" />
-        </video>
-      )}
+      <video
+        key={currentVideoFile.filename}
+        className="video"
+        playsInline
+        controls
+      >
+        <source src={currentVideoFile.filename} type="video/mp4" />
+      </video>
     </>
   );
 };
