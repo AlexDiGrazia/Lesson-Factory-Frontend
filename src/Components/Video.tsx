8e5eba83bd324234396616b627b44832f2fafb36
Alex Digrazia <alexndigrazia@hotmail.com>
2024-09-24 00:22:37 -0700

    added signedUrl <source> tags, also played around with ways to determine if Url is active or expired

--- src/Components/Video.tsx
+++ src/Components/Video.tsx
@@ -1,17 +1,47 @@
+// import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useVideoContext } from "../Providers/videoProvider";
+import { Requests } from "../API/Requests";
+// import { Requests } from "../API/Requests";
 
 const Video = () => {
-  const { currentVideo } = useVideoContext();
+  const { currentVideo, signedMp4Url, signedWebmUrl, sign } = useVideoContext();
+  const [refreshCount, setRefreshCount] = useState<number>(1);
+
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  if (videoRef.current !== null) {
+    videoRef.current.currentTime = 30;
+  }
+
+  const handleSeek = () => {
+    if (
+      // Checks if Link has expired or is still active (expiry in seconds > current date in second ?) refreshes link and component if expired
+      Number(signedMp4Url.split("?")[1].split("&")[0].split("=")[1]) <
+      Math.floor(Date.now() / 1000)
+    ) {
+      console.log("expired link");
+      sign(currentVideo.filename);
+      setRefreshCount(refreshCount + 1);
+    } else {
+      console.log("Still good");
+    }
+  };
 
   return (
     <>
-      <video key={currentVideo.filename} className="video" playsInline controls>
-        <source
-          src={`${import.meta.env.VITE_CLOUDFRONT_DISTRIBUTION}/${
-            currentVideo.filename
-          }`}
-          type="video/mp4"
-        />
+      <video
+        // ref={videoRef}
+        preload="auto"
+        key={currentVideo.filename}
+        className="video"
+        playsInline
+        controls
+        onSeeking={handleSeek}
+      >
+        {signedMp4Url !== "" && <source src={signedMp4Url} type="video/mp4" />}
+        {signedWebmUrl !== "" && (
+          <source src={signedWebmUrl} type="video/webm" />
+        )}
       </video>
     </>
   );
