f7dd2040feabfbb63b63f9d576730391f9e3a9bf
Alex Digrazia <alexndigrazia@hotmail.com>
2024-09-24 00:27:17 -0700

    created signedMp4Url and signedWebmUrl state, and then created sign() function that takes a single file name and creates two different signed Urls and sets them to state

--- src/Providers/videoProvider.tsx
+++ src/Providers/videoProvider.tsx
@@ -15,22 +15,50 @@ type TVideoContext = {
   setAllVideos: Dispatch<SetStateAction<TVideo[]>>;
   currentVideo: TVideo;
   setCurrentVideo: Dispatch<SetStateAction<TVideo>>;
+  signedMp4Url: string;
+  signedWebmUrl: string;
+  setSignedMp4Url: Dispatch<SetStateAction<string>>;
+  setSignedWebmUrl: Dispatch<SetStateAction<string>>;
+  sign: (filename: string) => Promise<void>;
 };
 
 const VideoContext = createContext<TVideoContext>({} as TVideoContext);
 
 export const VideoProvider = ({ children }: { children: ReactNode }) => {
+  console.log("video provider ran");
   const [allVideos, setAllVideos] = useState<TVideo[]>([]);
   const [currentVideo, setCurrentVideo] = useState<TVideo>({} as TVideo);
-
+  const [signedMp4Url, setSignedMp4Url] = useState<string>("");
+  const [signedWebmUrl, setSignedWebmUrl] = useState<string>("");
   const video_inLocalStorage = localStorage.getItem("videoLastWatched");
+
+  const cloudfrontDistribution = import.meta.env.VITE_CLOUDFRONT_DISTRIBUTION;
+
+  const sign = (filename: string) => {
+    return new Promise<void>(async (resolve, reject) => {
+      try {
+        await Requests.getSignedUrl(
+          `${cloudfrontDistribution}/${filename}.mp4`
+        ).then((res) => setSignedMp4Url(res.signedUrl));
+        await Requests.getSignedUrl(
+          `${cloudfrontDistribution}/${filename}.webm`
+        ).then((res) => setSignedWebmUrl(res.signedUrl));
+        resolve();
+      } catch (error) {
+        console.error(error);
+        reject();
+      }
+    });
+  };
 
   useEffect(() => {
     if (video_inLocalStorage) {
       setCurrentVideo(JSON.parse(video_inLocalStorage));
+      sign(JSON.parse(video_inLocalStorage).filename);
     } else {
       Requests.getFirstVideoInTable().then((res) => {
         localStorage.setItem("videoLastWatched", JSON.stringify(res[0]));
+        sign(res[0].filename);
         setCurrentVideo(res[0]);
       });
     }
@@ -43,6 +71,11 @@ export const VideoProvider = ({ children }: { children: ReactNode }) => {
         setAllVideos,
         currentVideo,
         setCurrentVideo,
+        signedMp4Url,
+        setSignedMp4Url,
+        signedWebmUrl,
+        setSignedWebmUrl,
+        sign,
       }}
     >
       {currentVideo && children}
