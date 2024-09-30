aa3a887d90e0621c1ff881b383f10df4bf00dc28
Alex Digrazia <alexndigrazia@hotmail.com>
2024-09-29 21:46:40 -0700

    add landing page text and login form

--- src/Routes/Root.tsx
+++ src/Routes/Root.tsx
@@ -1,7 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { useVideoContext } from "../Providers/videoProvider";
+import { useState } from "react";
+import { Login } from "../Components/login";
 
 export const Root = () => {
+  const [login, setLogin] = useState<boolean>(true);
   const navigate = useNavigate();
 
   const { currentVideo } = useVideoContext();
@@ -10,25 +13,37 @@ export const Root = () => {
     <>
       <nav>
         <ul>
-          <li>Login</li>
+          <li
+            className="login-btn"
+            onClick={() => {
+              console.log("click");
+              navigate(`app/${currentVideo.id}`);
+            }}
+          >
+            Login
+          </li>
         </ul>
       </nav>
-      <div className="radial-gradient-container">
-        <div className="radial-gradient one radial-gradient-dimension"></div>
-        <div className="radial-gradient two radial-gradient-dimension"></div>
-        <div className="radial-gradient three radial-gradient-dimension"></div>
-        <div className="radial-gradient four radial-gradient-dimension"></div>
-        <div className="radial-gradient five radial-gradient-dimension"></div>
-        <button
-          className="enter-site-btn"
-          onClick={() => {
-            console.log("click");
-            navigate(`app/${currentVideo.id}`);
-          }}
-        >
-          Enter Site
-        </button>
-      </div>
+      {login === false && (
+        <div>
+          <div className="radial-gradient-container">
+            <div className="radial-gradient one radial-gradient-dimension"></div>
+            <div className="radial-gradient two radial-gradient-dimension"></div>
+            <div className="radial-gradient three radial-gradient-dimension"></div>
+            <div className="radial-gradient four radial-gradient-dimension"></div>
+            <div className="radial-gradient five radial-gradient-dimension"></div>
+          </div>
+          <div className="text-container">
+            <h2>Welcome to the Lesson Factory!</h2>
+            <p>
+              The goal of this project is to present the Music Theory of guitar
+              in a digestible system, providing players of all levels with a
+              solid foundation to pursue whatever music they like.
+            </p>
+          </div>
+        </div>
+      )}
+      {login === true && <Login />}
     </>
   );
 };
