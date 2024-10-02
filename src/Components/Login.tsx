f05c52dbb3abfa61212b065af698907b20003eb0
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-01 18:47:23 -0700

    add submit button and SignUp CTA

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,11 +1,22 @@
 export const Login = () => {
+  const handleSubmit = () => {};
+
   return (
     <>
       <form className="login-form">
+        <p>
+          Don't have an account?{" "}
+          <span>
+            <strong>Sign up</strong>
+          </span>
+        </p>
         <label htmlFor="username"></label>
         <input type="text" name="username" placeholder="Username" />
         <label htmlFor="password"></label>
         <input type="text" name="password" placeholder="Password" />
+        <button type="submit" className="submit" onSubmit={handleSubmit}>
+          Submit
+        </button>
       </form>
     </>
   );
