db0642b274c3da5f05b34479eb91d239807febd6
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-01 20:40:15 -0700

    updated Login.tsx to have two different states. One for login and one for Signup

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,32 +1,52 @@
+import { useState } from "react";
 import { PasswordInput } from "./PasswordInput";
 
 export const Login = () => {
+  const [signup, setSignup] = useState<boolean>(false);
+
   const handleSubmit = () => {};
 
   return (
     <>
       <form className="login-form">
-        <p>
-          Don't have an account?{" "}
-          <span>
-            <strong>Sign up</strong>
-          </span>
-        </p>
-        <label htmlFor="username"></label>
-        <input
-          type="text"
-          name="username"
-          className="input-default"
-          placeholder="Username"
-        />
-        <PasswordInput placeholder="Password" />
-        {/* <label htmlFor="password"></label>
-        <input
-          type="text"
-          name="password"
-          className="input-default"
-          placeholder="Password"
-        /> */}
+        {signup === false && (
+          <>
+            <p>
+              Don't have an account?{" "}
+              <span onClick={() => setSignup(signup === false ? true : false)}>
+                <strong>Sign up</strong>
+              </span>
+            </p>
+            <label htmlFor="username"></label>
+            <input
+              type="text"
+              name="username"
+              className="input-default"
+              placeholder="Username"
+            />
+            <PasswordInput placeholder="Password" />
+          </>
+        )}
+        {signup === true && (
+          <>
+            <p>
+              Back to{" "}
+              <span onClick={() => setSignup(signup === false ? true : false)}>
+                <strong>login</strong>
+              </span>
+            </p>
+            <label htmlFor="username"></label>
+            <input
+              type="text"
+              name="username"
+              className="input-default"
+              placeholder="Username"
+            />
+            <PasswordInput placeholder="Password" />
+            <PasswordInput placeholder="Confirm password" />
+          </>
+        )}
+
         <button type="submit" className="submit" onSubmit={handleSubmit}>
           Submit
         </button>
