ce51c7ee72c695a58900ffc273c195c2a554184a
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-06 23:46:23 -0700

    added type TypeExistingUserLogin

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,12 @@
+import { Dispatch, SetStateAction } from "react";
+
 export type TVideo = {
   id: number;
   filename: string;
   title: string;
+};
+
+export type TypeExistingUserLogin = {
+  signup: boolean;
+  setSignup: Dispatch<SetStateAction<boolean>>;
 };
