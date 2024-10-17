6e205f3d7fb90b4fb20bfca1e53debb9dc87c340
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-17 02:41:13 -0700

    made this component it's own <form> element so it can stand alone when being used.  Also, so that it could have its own handleSubmit function

--- src/Components/ExistingUserLogin.tsx
+++ src/Components/ExistingUserLogin.tsx
@@ -1,26 +1,69 @@
 import { PasswordInput } from "./PasswordInput";
 import { TypeExistingUserLogin } from "../types";
+import { useVideoContext } from "../Providers/videoProvider";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Requests } from "../API/Requests";
+import toast from "react-hot-toast";
+import { useUserContext } from "../Providers/UserProvider";
 
 export const ExistingUserLogin = ({
   signup,
   setSignup,
 }: TypeExistingUserLogin) => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const navigate = useNavigate();
+  const { currentVideo } = useVideoContext();
+  const { setJWT } = useUserContext();
+
+  const clearLoginForm = () => {
+    setEmail("");
+    setPassword("");
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    Requests.login(email, password).then((res) => {
+      if (res.JWT) {
+        localStorage.setItem("JWT", res.JWT);
+        setJWT(res.JWT);
+        clearLoginForm();
+        navigate(`app/${currentVideo.id}`);
+      } else {
+        toast.error("One of your login credentials is incorrect");
+      }
+    });
+  };
   return (
     <>
-      <p>
-        Don't have an account?{" "}
-        <span onClick={() => setSignup(!signup)}>
-          <strong>Sign up</strong>
-        </span>
-      </p>
-      <label htmlFor="username"></label>
-      <input
-        type="text"
-        name="username"
-        className="input-default"
-        placeholder="Username"
-      />
-      <PasswordInput placeholder="Password" />
+      <form className="login-form" onSubmit={handleSubmit}>
+        <p>
+          Don't have an account?{" "}
+          <span onClick={() => setSignup(!signup)}>
+            <strong>Sign up</strong>
+          </span>
+        </p>
+        <label htmlFor="email"></label>
+        <input
+          type="text"
+          name="email"
+          className="input-default"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <PasswordInput
+          key={"login-password-input"}
+          placeholder="Password"
+          value={password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
+        />
+        <input type="submit" value="Submit" className="submit" />
+      </form>
     </>
   );
 };
