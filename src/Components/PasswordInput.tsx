fa15653b61a1e1961722356f076007489159df20
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-17 02:36:55 -0700

    added value and onChange props so component can be controlled by parent state.  Also added a Key value, but I think I did it wrong.  Needs to be fixed later

--- src/Components/PasswordInput.tsx
+++ src/Components/PasswordInput.tsx
@@ -3,8 +3,18 @@ import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
-export const PasswordInput = ({ placeholder }: { placeholder: string }) => {
-  const [inputType, setInputType] = useState<"text" | "password">("text");
+export const PasswordInput = ({
+  placeholder,
+  key,
+  value,
+  onChange,
+}: {
+  placeholder: string;
+  key: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) => {
+  const [inputType, setInputType] = useState<"text" | "password">("password");
   const [icon, setIcon] = useState<IconProp>(faEye);
 
   const togglePassword = () => {
@@ -16,14 +26,20 @@ export const PasswordInput = ({ placeholder }: { placeholder: string }) => {
 
   return (
     <>
-      <div className="input-default password-input-container">
+      <div key={key} className="input-default password-input-container">
         <FontAwesomeIcon
           icon={icon}
           className="password-toggle"
           onClick={togglePassword}
         />
         <label htmlFor="password"></label>
-        <input type={inputType} name="password" placeholder={placeholder} />
+        <input
+          type={inputType}
+          name="password"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
       </div>
     </>
   );
