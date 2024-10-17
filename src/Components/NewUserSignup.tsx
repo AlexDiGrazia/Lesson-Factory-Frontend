83d33e1bff703d8e4987e21d5d38f929ffe79a98
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-17 02:42:13 -0700

    made Component have its own <form> parent element so the component can stand on its own, and also have its own handleSubmit() function

--- src/Components/NewUserSignup.tsx
+++ src/Components/NewUserSignup.tsx
@@ -1,24 +1,69 @@
+import { useNavigate } from "react-router-dom";
 import { TypeExistingUserLogin } from "../types";
 import { PasswordInput } from "./PasswordInput";
+import { useVideoContext } from "../Providers/videoProvider";
+import { useState } from "react";
+import { Requests } from "../API/Requests";
 
 export const NewUserSignup = ({ signup, setSignup }: TypeExistingUserLogin) => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState<string>("");
+
+  const navigate = useNavigate();
+  const { currentVideo } = useVideoContext();
+
+  const clearSignupForm = () => {
+    setEmail("");
+    setPassword("");
+    setPasswordConfirmation("");
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("submission");
+    password === passwordConfirmation &&
+      Requests.signup(email, password, "USER").then(() => {
+        clearSignupForm();
+        navigate(`app/${currentVideo.id}`);
+      });
+  };
   return (
     <>
-      <p>
-        Back to{" "}
-        <span onClick={() => setSignup(!signup)}>
-          <strong>login</strong>
-        </span>
-      </p>
-      <label htmlFor="username"></label>
-      <input
-        type="text"
-        name="username"
-        className="input-default"
-        placeholder="Username"
-      />
-      <PasswordInput placeholder="Password" />
-      <PasswordInput placeholder="Confirm password" />
+      <form className="login-form" onSubmit={handleSubmit}>
+        <p>
+          Back to{" "}
+          <span onClick={() => setSignup(!signup)}>
+            <strong>login</strong>
+          </span>
+        </p>
+        <label htmlFor="email"></label>
+        <input
+          type="text"
+          name="email"
+          className="input-default"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <PasswordInput
+          key={"new-user-password"}
+          placeholder="Password"
+          value={password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
+        />
+        <PasswordInput
+          key={"new-user-confirm-password"}
+          placeholder="Confirm password"
+          value={passwordConfirmation}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPasswordConfirmation(e.target.value)
+          }
+        />
+        <input type="submit" value="Submit" className="submit" />
+      </form>
     </>
   );
 };
