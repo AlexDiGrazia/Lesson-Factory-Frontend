19ad0d738fe85274a9975c037ca545f2e262638c
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-17 02:40:05 -0700

    made <ExistingUserLogin /> and <NewUserSignup /> have their own <form> elements so I could remove <form> from this components jsx

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -1,34 +1,18 @@
 import { useState } from "react";
-// import { PasswordInput } from "./PasswordInput";
-import { useNavigate } from "react-router-dom";
-import { useVideoContext } from "../Providers/videoProvider";
 import { ExistingUserLogin } from "./ExistingUserLogin";
 import { NewUserSignup } from "./NewUserSignup";
 
 export const LoginForm = () => {
   const [signup, setSignup] = useState<boolean>(false);
 
-  const navigate = useNavigate();
-  const { currentVideo } = useVideoContext();
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log("submission");
-    navigate(`app/${currentVideo.id}`);
-  };
-
   return (
     <>
-      <form className="login-form" onSubmit={handleSubmit}>
-        {signup === false && (
-          <ExistingUserLogin signup={signup} setSignup={setSignup} />
-        )}
-        {signup === true && (
-          <NewUserSignup signup={signup} setSignup={setSignup} />
-        )}
-
-        <input type="submit" value="Submit" className="submit" />
-      </form>
+      {signup === false && (
+        <ExistingUserLogin signup={signup} setSignup={setSignup} />
+      )}
+      {signup === true && (
+        <NewUserSignup signup={signup} setSignup={setSignup} />
+      )}
     </>
   );
 };
