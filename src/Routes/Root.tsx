83f70b5fe6739629fff1b2c05780a2d4ab9f3273
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-17 20:46:44 -0700

    put all the landing page content into its own component, then set up conditional rendering based on queryParams being fed to it from <Main /> Router

--- src/Routes/Root.tsx
+++ src/Routes/Root.tsx
@@ -1,82 +1,14 @@
-import { useState } from "react";
-import { FlexboxCenter } from "../Components/FlexboxCenter";
 import { RadialGradient } from "../Components/RadialGradient";
-import { ExistingUserLogin } from "../Components/ExistingUserLogin";
-import { NewUserSignup } from "../Components/NewUserSignup";
 import "../CSS/utility.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { LandingPage } from "../Components/LandingPage";
+import { EmailVerificationSplash } from "../Components/emailVerificationSplash";
 
-export const Root = () => {
-  const [login, setLogin] = useState<boolean>(false);
-  const [signup, setSignup] = useState<boolean>(false);
-
+export const Root = ({ queryParam }: { queryParam: string }) => {
   return (
     <>
       <RadialGradient>
-        <nav>
-          <ul>
-            {signup === false && login === false && (
-              <li className="login-btn">
-                <span
-                  className="link-spacing link_hover"
-                  onClick={() => setLogin(true)}
-                >
-                  Login
-                </span>{" "}
-                |{" "}
-                <span
-                  className="link-spacing link_hover"
-                  onClick={() => setSignup(true)}
-                >
-                  Sign Up
-                </span>
-              </li>
-            )}
-            {(login === true || signup === true) && (
-              <li
-                className="link_hover"
-                onClick={() => {
-                  if (login === true) setLogin(false);
-                  if (signup === true) setSignup(false);
-                }}
-              >
-                <FontAwesomeIcon icon={faArrowLeft} />
-                Back
-              </li>
-            )}
-          </ul>
-        </nav>
-        {login === false && signup === false && (
-          <div className="text-container">
-            <h2>Welcome to the Lesson Factory!</h2>
-            <p>
-              The goal of this project is to present the Music Theory of guitar
-              in a digestible system, providing players of all levels with a
-              solid foundation to pursue whatever music they like.
-            </p>
-          </div>
-        )}
-        {login === true && (
-          <FlexboxCenter>
-            <ExistingUserLogin
-              login={login}
-              setLogin={setLogin}
-              signup={signup}
-              setSignup={setSignup}
-            />
-          </FlexboxCenter>
-        )}
-        {signup === true && (
-          <FlexboxCenter>
-            <NewUserSignup
-              login={login}
-              setLogin={setLogin}
-              signup={signup}
-              setSignup={setSignup}
-            />
-          </FlexboxCenter>
-        )}
+        {queryParam === "landing_page" && <LandingPage />}
+        {queryParam === "email_verification" && <EmailVerificationSplash />}
       </RadialGradient>
     </>
   );
