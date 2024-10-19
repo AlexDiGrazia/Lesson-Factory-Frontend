2d39c160ba1a193c1eb2db65d35a523794af11f0
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-19 00:37:37 -0700

    took all the contents of the 'Resend Email' <span>, put it into its own component, and the dropped that component in its place

--- src/Components/EmailVerificationSplash.tsx
+++ src/Components/EmailVerificationSplash.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { Requests } from "../API/Requests";
 import toast from "react-hot-toast";
 import { useState } from "react";
+import { ResendEmailSpan } from "./ResendEmailSpan";
 
 export const EmailVerificationSplash = () => {
   const [clickCount, setClickCount] = useState<number>(0);
@@ -17,25 +18,10 @@ export const EmailVerificationSplash = () => {
             <br /> and click "Verify Email"
           </p>
           <div className="flex align_items_center">
-            <span
-              className="semi-bold onClick"
-              onClick={() => {
-                setClickCount(clickCount + 1);
-                if (clickCount >= 5)
-                  toast.success(
-                    "Sorry, you've been rate-limited.  If you still can't find the email, please try again try later."
-                  );
-                const email = localStorage.getItem("email");
-                const id = Number(localStorage.getItem("userId"));
-                if (!email || !id)
-                  return new Error("Email or id not found in local storage");
-                Requests.resendVerificationEmail(email, id).then(() => {
-                  toast.success("New email sent! Please check your inbox");
-                });
-              }}
-            >
-              Resend Link
-            </span>
+            <ResendEmailSpan
+              clickCount={clickCount}
+              setClickCount={setClickCount}
+            />
             <button
               className="semi-bold"
               onClick={() => {
