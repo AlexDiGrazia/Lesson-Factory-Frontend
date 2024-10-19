fbaac74451bc5ce49e6f4b94daa19aca0413032c
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-19 00:35:35 -0700

    took the <span> contents for the Resend Email link and put it into its own component so I could re-use it on both the EmailVerificationSplash page and the login page as a Toast pop-up

---
+++ src/Components/ResendEmailSpan.tsx
@@ -0,0 +1,38 @@
+import { Dispatch, SetStateAction, useState } from "react";
+import toast from "react-hot-toast";
+import { Requests } from "../API/Requests";
+
+type ResendEmailSpanProps = {
+  clickCount: number;
+  setClickCount: Dispatch<SetStateAction<number>>;
+};
+
+export const ResendEmailSpan = ({
+  clickCount,
+  setClickCount,
+}: ResendEmailSpanProps) => {
+  return (
+    <>
+      <span
+        className="semi-bold onClick toast-resend_link-span"
+        onClick={() => {
+          setClickCount((prev) => prev + 1);
+          if (clickCount >= 5)
+            toast.error(
+              "Sorry, you've been rate-limited.  If you still can't find the email, please try the Resend Email link again later.",
+              { duration: 7000 }
+            );
+          const email = localStorage.getItem("email");
+          const id = Number(localStorage.getItem("userId"));
+          if (!email || !id)
+            return new Error("Email or id not found in local storage");
+          Requests.resendVerificationEmail(email, id).then(() => {
+            toast.success("New email sent! Please check your inbox");
+          });
+        }}
+      >
+        Resend Link
+      </span>
+    </>
+  );
+};
