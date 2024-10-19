1734d5b8465f916d27e7096de22aaa4a9d51c51a
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-19 00:36:49 -0700

    added logic to check if a user email is verified, and to send a pop-up asking them to verify their email if they aren't

--- src/Components/ExistingUserLogin.tsx
+++ src/Components/ExistingUserLogin.tsx
@@ -5,10 +5,24 @@ import { useState } from "react";
 import { Requests } from "../API/Requests";
 import toast from "react-hot-toast";
 import { useUserContext } from "../Providers/UserProvider";
+import { jwtDecode } from "jwt-decode";
+import { ResendEmailSpan } from "./ResendEmailSpan";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+
+type JwtPayload = {
+  id: number;
+  email: string;
+  role: "ADMIN" | "USER";
+  emailVerified: boolean;
+  iat: number;
+};
 
 export const ExistingUserLogin = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [resendEmailSpanClickCount, setResendEmailSpanClickCount] =
+    useState<number>(0);
 
   const navigate = useNavigate();
   const { currentVideo } = useVideoContext();
@@ -23,10 +37,51 @@ export const ExistingUserLogin = () => {
     e.preventDefault();
     Requests.login(email, password).then((res) => {
       if (res.JWT) {
-        localStorage.setItem("JWT", res.JWT);
-        setJWT(res.JWT);
-        clearLoginForm();
-        navigate(`/app/${currentVideo.id}`);
+        const userObject = jwtDecode<JwtPayload>(res.JWT);
+        const emailIsVerified = userObject.emailVerified === true;
+        if (emailIsVerified) {
+          localStorage.setItem("JWT", res.JWT);
+          setJWT(res.JWT);
+          clearLoginForm();
+          navigate(`/app/${currentVideo.id}`);
+        } else {
+          toast(
+            (t) => (
+              <div style={{ display: "flex" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    marginRight: "13px",
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={faTriangleExclamation}
+                    className="alert_icon"
+                  />
+                </div>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <span style={{ marginBottom: "3px" }}>
+                    Please verify your email before logging in.
+                  </span>
+                  <span onClick={() => toast.dismiss(t.id)}>
+                    <ResendEmailSpan
+                      clickCount={resendEmailSpanClickCount}
+                      setClickCount={setResendEmailSpanClickCount}
+                    />
+                  </span>
+                </div>
+              </div>
+            ),
+            {
+              duration: 120000,
+              style: {
+                maxWidth: "50%",
+                padding: "8px 10px 8px 1px",
+              },
+            }
+          );
+        }
       } else {
         toast.error("One of your login credentials is incorrect");
       }
