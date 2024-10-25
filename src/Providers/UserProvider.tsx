645901132530face95c4d75a5d71cfd3cdeceafc
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-24 22:29:47 -0700

    setJWT with value from localStorage upon page refresh

--- src/Providers/UserProvider.tsx
+++ src/Providers/UserProvider.tsx
@@ -19,12 +19,9 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
   const [JWT, setJWT] = useState<string>("");
 
   useEffect(() => {
-    // consder if (JWT in local storage, setJWET(jwt in local storage)) instead of if (JWT === "")
-    if (JWT === "") {
-      const storedJWT = localStorage.getItem("JWT");
-      if (storedJWT) {
-        setJWT(storedJWT);
-      }
+    const jwtFromStorage = localStorage.getItem("JWT");
+    if (JWT === "" && jwtFromStorage) {
+      setJWT(jwtFromStorage);
     }
   });
 
