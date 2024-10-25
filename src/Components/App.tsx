a0f3874ca0cc4b0e885767cea05c972f2e9f3fbc
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-24 22:17:46 -0700

    moved fetching allVideos data to the VideoContext Provider. Also added allVideos.length > 0 conditional rendering to the .map()

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,33 +1,27 @@
 /* eslint-disable no-undef */
 import { useEffect } from "react";
-import { Requests } from "../API/Requests";
 import { useVideoContext } from "../Providers/videoProvider";
 import Video from "./Video";
 import VideoTitle from "./VideoTitle";
 import "../CSS/App.css";
 import { NewPost } from "./NewPost";
-import { useUserContext } from "../Providers/UserProvider";
 
 const App = () => {
-  const { allVideos, setAllVideos } = useVideoContext();
-  const { JWT } = useUserContext();
-
-  useEffect(() => {
-    Requests.getAllVideos(JWT).then(setAllVideos);
-  }, []);
+  const { allVideos } = useVideoContext();
 
   return (
     <>
       <div className="wrapper">
         <div className="menu">
-          {allVideos?.map((obj) => (
-            <VideoTitle
-              key={`video_key_${obj.id}`}
-              title={obj.title}
-              filename={obj.filename}
-              id={obj.id}
-            />
-          ))}
+          {allVideos.length > 0 &&
+            allVideos?.map((obj) => (
+              <VideoTitle
+                key={`video_key_${obj.id}`}
+                title={obj.title}
+                filename={obj.filename}
+                id={obj.id}
+              />
+            ))}
         </div>
         <div className="video-panel">
           <Video />
