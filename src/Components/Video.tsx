0a6460c9608d1612669812b25e468b0db135ba03
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-28 23:45:24 -0700

    added logic to blur video if user is unsubscribed.  Added accompanying logic to make sure Component has a JWT token in case of user page refresh

--- src/Components/Video.tsx
+++ src/Components/Video.tsx
@@ -1,9 +1,15 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useVideoContext } from "../Providers/videoProvider";
+import { useUserContext } from "../Providers/UserProvider";
+import { jwtDecode } from "jwt-decode";
+import { JwtPayload } from "../types";
 
 const Video = () => {
-  const { currentVideo, signedMp4Url, signedWebmUrl, sign } = useVideoContext();
   const [refreshCount, setRefreshCount] = useState<number>(1);
+  const [subscribed, setSubscribed] = useState<boolean>(false);
+
+  const { currentVideo, signedMp4Url, signedWebmUrl, sign } = useVideoContext();
+  const { JWT } = useUserContext();
 
   const videoRef = useRef<HTMLVideoElement | null>(null);
   if (videoRef.current !== null) {
@@ -24,6 +30,19 @@ const Video = () => {
     }
   };
 
+  useEffect(() => {
+    if (JWT) {
+      setSubscribed(jwtDecode<JwtPayload>(JWT).subscribed);
+    } else {
+      const jwtFromStorage = localStorage.getItem("JWT");
+      if (!jwtFromStorage) {
+        console.error({ error: "No JWT in localStorage" });
+        return;
+      }
+      setSubscribed(jwtDecode<JwtPayload>(jwtFromStorage).subscribed);
+    }
+  }, []);
+
   return (
     <>
       <video
@@ -32,8 +51,10 @@ const Video = () => {
         key={currentVideo.filename}
         className="video"
         playsInline
+        // controls={user.subscribed === true}
         controls
         onSeeking={handleSeek}
+        style={{ filter: !subscribed ? "blur(2px)" : "none" }}
       >
         {signedMp4Url !== "" && <source src={signedMp4Url} type="video/mp4" />}
         {signedWebmUrl !== "" && (
