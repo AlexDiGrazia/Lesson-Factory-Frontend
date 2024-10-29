ddda93d163a6d2deff04d6b541ffe80479869385
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-29 02:48:42 -0700

    lifted Subscribed state to userProvider so it can be set from multiple child components - specifically after a user newly purchases a subscription so the videos are no longer locked when they then return to the videos dashboard from the payment confirmation page

--- src/Providers/UserProvider.tsx
+++ src/Providers/UserProvider.tsx
@@ -11,12 +11,15 @@ import {
 type TUserContext = {
   JWT: string;
   setJWT: Dispatch<SetStateAction<string>>;
+  subscribed: boolean;
+  setSubscribed: Dispatch<SetStateAction<boolean>>;
 };
 
 const UserContext = createContext<TUserContext>({} as TUserContext);
 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
   const [JWT, setJWT] = useState<string>("");
+  const [subscribed, setSubscribed] = useState<boolean>(false);
 
   const jwtFromStorage = localStorage.getItem("JWT");
   const pageWasRefreshed = JWT === "" && jwtFromStorage;
@@ -28,7 +31,7 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
   });
 
   return (
-    <UserContext.Provider value={{ JWT, setJWT }}>
+    <UserContext.Provider value={{ JWT, setJWT, subscribed, setSubscribed }}>
       {children}
     </UserContext.Provider>
   );
